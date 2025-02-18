c7e8140db00551d446d0fd99cb9a2bb98821adf8
backslash-ux <info@backslash.id>
2025-02-18 15:08:27 +0700

    add YOUTUBE_COOKIES to config.py

--- backend/config.py
+++ backend/config.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     APP_HOST: str
     APP_PORT: int
     DATABASE_URL: str
-
+    YOUTUBE_COOKIES: str
     model_config = SettingsConfigDict(
         env_file=str(BASE_DIR / ".env"),
         env_file_encoding="utf-8"
